bd65c1094c790e8e248038e08301226d5ae79bad
nachosmillescas <79314141+nachosmillescas@users.noreply.github.com>
2022-01-02 19:19:26 +0100

    Update script.py

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ import sys
 from subprocess import call
 
 	
-call(["export", "GROUP_NUMBER=33"])
+os.system("export GROUP_NUMBER=33")
 call(["sudo", "rm", "-r", "CDPS_P2"] )
 call(["git", "clone", "https://github.com/nachosmillescas/CDPS_P2"] )
 call(["sudo", "apt-get", "install", "python"] )
