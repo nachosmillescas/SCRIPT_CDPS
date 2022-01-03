5342e2d3320de63697542890a5687d7f50170cfa
nachosmillescas <79314141+nachosmillescas@users.noreply.github.com>
2022-01-03 19:18:35 +0100

    Update script.py

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ from subprocess import call
 def modifHTML():
   h=open("practica_creativa2/bookinfo/src/productpage/templates/productpage.html", "r")
   aux=open("practica_creativa2/bookinfo/src/productpage/templates/auxiliar.html", "w+")
-  formato=os.environ.get('GROUP_NUMBER')
+  formato=4#os.environ.get('GROUP_NUMBER')
   blockTitle = "{% block title %}"
   endBlock = "{% endblock %}"
   for line in h:
